61c3ea2e9dc0699bef955d28e9cd946c657bb1ac
Melinda Burke <melccb@gmail.com>
2018-08-20 02:57:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-if word.match(/^[aeiouAEIOU]+\w/) then
+  if word.match(/^[aeiouAEIOU]+\w/) then
     return true
   else
     return false
-end
+  end
 
 end
 
@@ -16,7 +16,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+ if text.match(/^[A-Z].+[\.!?]$/) then
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
